9443fb878c1b3b0a722a16e9782f7aa8f3ca8b00
Logon Aluno <logonaluno@local.com>
2019-09-09 23:00:36 -0300

    Second Commit

--- app/src/main/java/br/com/fernandavedovello/pokedex/view/form/FormPokemonActivity.kt
+++ app/src/main/java/br/com/fernandavedovello/pokedex/view/form/FormPokemonActivity.kt
@@ -3,8 +3,17 @@ package br.com.fernandavedovello.pokedex.view.detail.form
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.fernandavedovello.pokedex.R
+import br.com.fernandavedovello.pokedex.model.Pokemon
+import br.com.fernandavedovello.pokedex.view.form.FormPokemonViewModel
+import com.squareup.picasso.Picasso
+import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class FormPokemonActivity : AppCompatActivity() {
+
+    val formPokemonViewModel: FormPokemonViewModel by viewModel()
+    val picasso: Picasso by inject()
+    lateinit var pokemon : Pokemon
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
